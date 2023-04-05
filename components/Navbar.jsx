8de7ff109fa7ed9680abf7a26e034f1856088f22
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-04-05 10:40:59 -0700

    nav item and navbar components created

---
+++ components/Navbar.jsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import React, { useState } from "react";
+import NavItem from "./NavItem";
+
+const MENU_LIST = [
+  { text: "Home", href: "/" },
+  { text: "Professional Experience", href: "/experience" },
+  { text: "Personal Projects", href: "/projects" },
+  { text: "About", href: "/about" },
+];
+const Navbar = () => {
+  const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
+
+  return (
+    <header>
+      <nav className={`nav`}>
+        <Link href={"/"}>
+            <h1>Amanda Godsey</h1>
+        </Link>
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+          {MENU_LIST.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+            >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
