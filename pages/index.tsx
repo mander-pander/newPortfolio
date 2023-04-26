ecab2d7de3dac7a99ff02e74b68c1fe44cd610f8
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-04-26 09:41:13 -0700

    added destination links for socials

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import avatar from '../assets/avatar-removebg-preview.png';
 import styles from '../styles/Home.module.css';
 import { IoLogoLinkedin, IoLogoGithub, IoMdPin } from "react-icons/io";
@@ -13,28 +14,34 @@ export default function Home() {
         src={avatar}
         className={styles.avatar}
       />
-
-      <h1>Amanda Godsey</h1>
-      <h3>Full Stack Web Developer</h3>
-      <div>
-        <IoLogoLinkedin />
-        <IoLogoGithub />
+      <div className={styles.info}>
+        <h1>Amanda Godsey</h1>
+        <h3>Full Stack Web Developer</h3>
+        <div>
+          <Link href="https://www.linkedin.com/in/amanda-godsey/">
+            <IoLogoLinkedin />
+          </Link>
+          <Link href="https://github.com/mander-pander">
+          <IoLogoGithub />
+          </Link>
+        </div>
+        <h5><IoMdPin /> Sacramento, CA</h5>
       </div>
-      <h5><IoMdPin /> Sacramento, CA</h5>
 
-      <section className={styles.skills}>
-        <SiReact />
-        <SiNextdotjs />
-        <SiTypescript />
-        <SiJavascript />
-        <SiNodedotjs />
-        <SiHtml5 />
-        <SiCss3 />
-        <SiExpress />
-        <SiPostgresql />
-        <SiGit />
-        <SiGithub />
-      </section>
+      <h3 className={styles.title}>Skills</h3>
+      <div className={styles.skills}>
+        <p><SiReact /> React</p>
+        <p><SiNextdotjs /> Next.js</p>
+        <p><SiTypescript /> Typescript</p>
+        <p><SiJavascript /> Javascript</p>
+        <p><SiNodedotjs /> Node.js</p>
+        <p><SiHtml5 /> HTML</p>
+        <p><SiCss3 /> CSS</p>
+        <p><SiExpress /> Express.js</p>
+        <p><SiPostgresql /> PostgresSQL</p>
+        <p><SiGit /> Git</p>
+        <p><SiGithub /> Github</p>
+      </div>
 
     </div>
   )
