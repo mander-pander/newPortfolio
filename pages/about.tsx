6cec4903dba588ecbc00404c46c41127d08b76c2
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-08-22 11:29:02 -0400

    about page

---
+++ pages/about.tsx
@@ -0,0 +1,20 @@
+import styles from "../styles/About.module.css";
+
+export default function About() {
+    return (
+        <div className={styles.main}>
+            <div className={styles.info}>
+                <h1>Who</h1>
+                <p>
+                    Having a joy for creating solutions and a desire for community I joined the
+                    Full Stack Web Dev bootcamp at Devmountain in 2022. I enjoy the challenges
+                    and open source world of developing. Outside of web development I spend my
+                    time backpacking, rock climbing, reading and knitting.
+                </p>
+                <h1>My Experience</h1>
+                <p></p>
+
+            </div>
+        </div>
+    )
+}
